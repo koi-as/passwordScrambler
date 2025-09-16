8c672eadf4e395769e1f5fd44e52e0c653c7ab66
koi-as <lightningblade.901@gmail.com>
2025-09-16 13:14:26 -0400

    Started by importing libraries tkinter and random. Set up popup window using tkinter Tk() module. Set up basic window allowing user entry, button that sends hello to command line, and entry line for output.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# passwordScrambler
+# Password Scarmabler
+### By Koi
+
+## Description
 This python program will accept a text input in a separate window, scramble the letters, then change the characters used to something random.
---
+++ scrambler.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import *
+import random
+
+# set up a window to display text and obtain user input using tk win
+# accept user input as string
+# take string and randomize placement of characters
+# take new randomized string, and randomize cahracters within the string
+# use keyboard only characters so the scrambled password can be used normally
+# a-z, A-Z, 0-9, !@#$%^&*?
+# that should be a fine enough set of characters
+
+# create the window object and set size and title
+win=tk.Tk()
+win.geometry('750x750')
+win.title('The Great Scrambler')
+
+# initialize vars
+password=''
+product=''
+
+# set up function
+def scramble():
+    print('hello')
+
+# set up window elements
+Entry(win, textvariable=password, width=20, font=('Courier', 25), relief=GROOVE).place(relx=.5, rely=.2, anchor=CENTER)
+Entry(win, textvariable=product, width=20, font=('Courier', 25), relief=GROOVE).place(relx=.5, rely=.8, anchor=CENTER)
+Button(win, width=10, text='SCRAMBLE IT!', command=scramble, relief=GROOVE, bg='orange').place(relx=.5, rely=.5, anchor=CENTER)
+
+win.mainloop()
